64f98c0002db05e7a085a474dcc0a9088abdd939
Miłosz Pasek <malzusiek666@gmail.com>
2022-04-12 19:35:33 +0200

    deleting redux files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
 
 import App from "./App"
-import { store } from "./store"
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.querySelector("#root"))
