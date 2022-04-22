4105c42520202e8fe48a4fc7b4a783a1f45bbd9c
Miłosz Pasek <malzusiek666@gmail.com>
2022-04-22 19:35:20 +0200

    redirection to pokelist from root path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react"
 import MainPage from "./pages/MainPage"
 import About from "./pages/About"
 import { Footer, NavBar, PokeInfo } from "./components"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
 
 const App = () => {
   return (
@@ -10,6 +15,7 @@ const App = () => {
       <Router>
         <NavBar />
         <Routes>
+          <Route path="/" element={<Navigate replace to="/pokelist/1" />} />
           <Route path="/pokelist/:page" element={<MainPage />} />
           <Route path="/about" element={<About />} />
           <Route path="/pokemon/:id" element={<PokeInfo />} />
