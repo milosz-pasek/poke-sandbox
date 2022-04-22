fc472a721daf3da8257ae6ed6f41ea262bb2062c
Miłosz Pasek <malzusiek666@gmail.com>
2022-04-22 20:18:58 +0200

    fixed navbar routing bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,35 +7,25 @@ import Typography from "@mui/material/Typography"
 import Menu from "@mui/material/Menu"
 import MenuIcon from "@mui/icons-material/Menu"
 import Container from "@mui/material/Container"
-import Avatar from "@mui/material/Avatar"
 import Button from "@mui/material/Button"
-import Tooltip from "@mui/material/Tooltip"
 import MenuItem from "@mui/material/MenuItem"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const pages = [
-  { name: "PokeList", link: "pokelist/1" },
+  { name: "PokeList", link: "" },
   { name: "About", link: "about" }
 ]
-const settings = ["Profile", "Account", "Dashboard", "Logout"]
-
 export const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null)
-  const [anchorElUser, setAnchorElUser] = React.useState(null)
+
+  const navigate = useNavigate()
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
   }
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget)
-  }
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
   }
 
   return (
@@ -81,10 +71,11 @@ export const NavBar = () => {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link to={page.link}>{page.name}</Link>
-                  </Typography>
+                <MenuItem
+                  key={page.name}
+                  onClick={() => navigate(`/${page.link}`)}
+                >
+                  <Typography textAlign="center">{page.name}</Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -101,48 +92,13 @@ export const NavBar = () => {
             {pages.map((page) => (
               <Button
                 key={page.name}
-                onClick={handleCloseNavMenu}
+                onClick={() => navigate(`/${page.link}`)}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  to={page.link}
-                >
-                  {page.name}
-                </Link>
+                {page.name}
               </Button>
             ))}
           </Box>
-          {/* 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
