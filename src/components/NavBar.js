b76c76aa49f94a386f88af477b1a3ae7f761099a
Miłosz Pasek <malzusiek666@gmail.com>
2022-04-24 19:48:04 +0200

    added search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,9 +11,13 @@ import Button from "@mui/material/Button"
 import MenuItem from "@mui/material/MenuItem"
 import { useNavigate } from "react-router-dom"
 import { pages } from "../configs/navigationConfig"
+import TextField from "@mui/material/TextField"
+import SearchIcon from "@mui/icons-material/Search"
+import { InputAdornment } from "@mui/material"
 
 export const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null)
+  const [searchValue, setSearchValue] = React.useState("")
 
   const navigate = useNavigate()
 
@@ -25,17 +29,31 @@ export const NavBar = () => {
     setAnchorElNav(null)
   }
 
+  const handleSearchSubmit = (e) => {
+    if (searchValue) {
+      e.preventDefault()
+      navigate(`/pokemon/${searchValue.toLowerCase()}`)
+      setSearchValue("")
+    } else {
+      e.preventDefault()
+    }
+  }
+
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ bgcolor: "#F6F6F6" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              color: "text.secondary"
+            }}
           >
-            LOGO
+            Poke Sandbox
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -78,20 +96,38 @@ export const NavBar = () => {
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none", color: "text.secondary" }
+            }}
           >
-            LOGO
+            Poke Sandbox
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map(({ name, path }) => (
               <Button
                 key={name}
                 onClick={() => navigate(path)}
-                sx={{ my: 2, color: "white", display: "block" }}
+                sx={{ my: 2, color: "text.secondary", display: "block" }}
               >
                 {name}
               </Button>
             ))}
+          </Box>
+          <Box
+            component="form"
+            onSubmit={handleSearchSubmit}
+            sx={{ display: "flex", blockSize: "min-content" }}
+          >
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Find Pokemon"
+              placeholder="eg. 'Pikachu' or '25'"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              variant="standard"
+            />
           </Box>
         </Toolbar>
       </Container>
